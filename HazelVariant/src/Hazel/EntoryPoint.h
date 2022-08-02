392e6e8ed29959ede08c6a2115dc71e30c59e3b6
dionysoslai <laiwugong@tencent.com>
2022-08-02 20:28:11 +0800

    add spdlog submodule

--- HazelVariant/src/Hazel/EntoryPoint.h
+++ HazelVariant/src/Hazel/EntoryPoint.h
@@ -1,12 +1,16 @@
 #pragma once
-#include "Application.h"
 
 #ifdef HZ_PLATFOR_WINDOWS
 extern Hazel::Application* Hazel::CreateAppliation();
 
 int main(int argc, char** argv)
 {
-	printf("Welcome Hazel Engin！！");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_INFO("Hello");
+	
+
+	//printf("Welcome Hazel Engin！！");
 	auto app = Hazel::CreateAppliation();
 	app->Run();
 	delete app;
