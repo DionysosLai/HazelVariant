9a52d455bed1a0cb9565fd8a028b781e1326931b
dionysoslai <laiwugong@tencent.com>
2022-08-05 20:26:38 +0800

    building system

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+workspace "HazelVariant"
+	architecture "x64"
+
+	configurations 
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "HazelVariant"
+	location "HazelVariant"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir ..   "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir ..   "/%{prj.name}")
+
+	files 
+	{ 
+		"%{prj.name}/src/**.h", 
+		"%{prj.name}/src/**.cpp",
+	}
+
+	include 
+	{
+		"%{prj.name}/vendor/spdlog/include",
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"HZ_PLATFOR_WINDOWS",
+			"HZ_BUILD_DLL"
+		}
+
+		postbuildcommands 
+		{ 
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Debug"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Debug"
+		defines "HZ_DIST"
+		optimize "On"
+	
