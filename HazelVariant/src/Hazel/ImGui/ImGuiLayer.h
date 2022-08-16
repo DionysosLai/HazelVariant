de2ac54e74e705ef52ba3b582b018db7b80e183b
dionysoslai <laiwugong@tencent.com>
2022-08-16 19:45:52 +0800

    add ImGuiLayer Event

--- HazelVariant/src/Hazel/ImGui/ImGuiLayer.h
+++ HazelVariant/src/Hazel/ImGui/ImGuiLayer.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Hazel/Layer.h"
+#include "Hazel/Events/MouseEvent.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/KeyEvent.h"
 
 namespace Hazel {
 
@@ -12,6 +15,16 @@ namespace Hazel {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressdEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEventEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEventEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEventEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEventEvent(WindowResizeEvent& e);
+		bool OnWindowCloseEventEvent(WindowCloseEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
