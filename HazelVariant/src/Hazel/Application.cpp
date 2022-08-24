9801ec051a1143f4a0e9d442c8da77842ddd5034
dionysoslai <laiwugong@tencent.com>
2022-08-24 09:27:27 +0800

    add Context and OpenGLContext

--- HazelVariant/src/Hazel/Application.cpp
+++ HazelVariant/src/Hazel/Application.cpp
@@ -31,7 +31,7 @@ namespace Hazel {
 	{
 		while (m_Running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0.1, 0.1 , 0.1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			
 			for (Layer* layer : m_LayerStack)
