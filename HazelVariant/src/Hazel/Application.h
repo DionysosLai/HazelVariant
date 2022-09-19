b3a813506323386b53a37a1b1d917650850c6f7d
dionysoslai <laiwugong@tencent.com>
2022-09-19 10:56:02 +0800

    add orthographic camera

--- HazelVariant/src/Hazel/Application.h
+++ HazelVariant/src/Hazel/Application.h
@@ -12,6 +12,7 @@
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
 #include "Hazel/Renderer/VertexArray.h"
+#include "Hazel/Renderer/OrthographicCamera.h"
 
 namespace Hazel {
 	class HAZEL_API Application
@@ -36,12 +37,13 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		std::unique_ptr<Shader> m_Shader;
+		std::shared_ptr<Shader> m_Shader;
 		std::shared_ptr<VertexArray> m_VertexArray;
 
 		std::shared_ptr<Shader> m_BlueShader;
 		std::shared_ptr<VertexArray> m_SquareVA;
 
+		OrthographicCamera m_Camera;
 	private:
 		static Application* s_Instance;
 	};
