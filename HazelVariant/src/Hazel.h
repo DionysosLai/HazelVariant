41ead1950ef33a671c04647105e4fea3ccdb4d7c
dionysoslai <laiwugong@tencent.com>
2022-10-08 12:59:27 +0800

    move application to sandbox

--- HazelVariant/src/Hazel.h
+++ HazelVariant/src/Hazel.h
@@ -15,6 +15,18 @@
 #include "Hazel/Events/KeyEvent.h"
 #include "Hazel/Events/MouseEvent.h"
 
+// ---Renderer------------------------
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/VertexArray.h"
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+// -----------------------------------
+
+
 #include "Hazel/ImGui/ImGuiLayer.h"
 // -----Entry Point-------------------------------
 #include "Hazel/EntoryPoint.h"
