6e0b19924d29975ad48b21748a89990fc62e204b
dionysoslai <laiwugong@tencent.com>
2022-10-10 14:37:08 +0800

    Moved OpenGL shader implementation to OpenGLShader class + added more uniform functions

--- HazelVariant/src/Hazel/Renderer/Renderer.cpp
+++ HazelVariant/src/Hazel/Renderer/Renderer.cpp
@@ -2,6 +2,7 @@
 #include "Renderer.h"
 
 #include "RenderCommand.h"
+#include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Hazel {
 	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
@@ -20,8 +21,8 @@ namespace Hazel {
 		const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
