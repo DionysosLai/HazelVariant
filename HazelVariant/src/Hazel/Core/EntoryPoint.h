8470d7055f950127a1f84a0ad04d824f47519566
dionysoslai <laiwugong@tencent.com>
2022-10-17 19:25:14 +0800

    Initial instrumentation work

--- HazelVariant/src/Hazel/Core/EntoryPoint.h
+++ HazelVariant/src/Hazel/Core/EntoryPoint.h
@@ -6,14 +6,18 @@ extern Hazel::Application* Hazel::CreateAppliation();
 int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
-	HZ_CORE_WARN("Initialized Log!");
-	HZ_INFO("Hello");
-	
 
-	//printf("Welcome Hazel Engin！！");
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
 	auto app = Hazel::CreateAppliation();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
 	app->Run();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Shutdown.json");
 	delete app;
+	HZ_PROFILE_END_SESSION();
 }
 #else
 #error Hazel only support Windows!
