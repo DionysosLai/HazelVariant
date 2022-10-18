e71aff961ae34709a0eff8a5b968f7a6ed5fbfe9
dionysoslai <laiwugong@tencent.com>
2022-10-18 10:17:07 +0800

    Move Sandbox ExampleLayer to separate file

--- HazelVariant/src/Hazel/Renderer/Buffer.h
+++ HazelVariant/src/Hazel/Renderer/Buffer.h
@@ -110,7 +110,7 @@ namespace Hazel {
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
-		static VertexBuffer* Create(float* vertices, uint32_t size);
+		static Ref<VertexBuffer> Create(float* vertices, uint32_t size);
 		virtual const BufferLayout& GetLayout() const = 0;
 		virtual void SetLayout(const BufferLayout& layout) = 0;
 	};
@@ -125,7 +125,7 @@ namespace Hazel {
 
 		virtual uint32_t GetCount() const = 0;
 
-		static IndexBuffer* Create(uint32_t* indices, uint32_t size);
+		static Ref<IndexBuffer>Create(uint32_t* indices, uint32_t size);
 	};
 
 
