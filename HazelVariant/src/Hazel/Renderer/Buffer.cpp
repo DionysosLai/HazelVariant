db9cce35ac72f84e5163f7262831b4e4ca4c6b1b
dionysoslai <laiwugong@tencent.com>
2022-10-20 19:47:43 +0800

    Added basic batching to Renderer2D

--- HazelVariant/src/Hazel/Renderer/Buffer.cpp
+++ HazelVariant/src/Hazel/Renderer/Buffer.cpp
@@ -4,6 +4,18 @@
 #include "Platform/OpenGL/OpenGLBuffer.h"
 
 namespace Hazel {
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLVertexBuffer>(size);
+		}
+
+		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
@@ -16,12 +28,12 @@ namespace Hazel {
 		return nullptr;
 	}
 
-	Ref <IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	Ref <IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t count)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLIndexBuffer>(indices, size);
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLIndexBuffer>(indices, count);
 		}
 
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
--- HazelVariant/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ HazelVariant/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -9,7 +9,8 @@ namespace Hazel {
 		virtual void Init();
 		virtual void SetClearColor(const glm::vec4& color);
 		virtual void Clear();
-		virtual void DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray);
+		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) override;
+
 		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
 	private:
 
