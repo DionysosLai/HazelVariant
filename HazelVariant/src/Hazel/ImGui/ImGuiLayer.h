9df1d76987c8fc2b38c15e83a39fbbdf4701dc5c
dionysoslai <laiwugong@tencent.com>
2022-10-24 20:24:16 +0800

    Imgui handling mouse and keyboards events (#219)

--- HazelVariant/src/Hazel/ImGui/ImGuiLayer.h
+++ HazelVariant/src/Hazel/ImGui/ImGuiLayer.h
@@ -13,6 +13,7 @@ namespace Hazel {
 		~ImGuiLayer() = default;
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
+		virtual void OnEvent(Event& e) override;
 
 		void Begin();
 		void End();
